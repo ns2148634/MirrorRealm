f1243d6b2c7f59f2a24a3f188f1678a07f68d4e9
jason chen <ewn.utt@gmail.com>
2026-04-01 21:57:52 +0800

    修正 Express 路由萬用字元報錯

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.use(express.static(join(__dirname, '../dist')));
 // 這條路由一定要放在所有 /api/... 路由的最下方
 // 新寫法 (完美避開版本衝突 👇)
 app.get(/.*/, (req, res) => {
-    res.sendFile(path.join(__dirname, '../dist/index.html'));
+    res.sendFile(join(__dirname, '../dist/index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
